79fc567a60737011eb9b547e30107f49477b0f74
Stivin <68162679+OrlovSA@users.noreply.github.com>
2020-12-25 11:25:23 +0500

    DZ_3.1-3.4

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -8,3 +8,31 @@
 Подсказка: задачу решите обязательно с применением 'соленого' хеширования
 Можете условжнить задачу, реализовав ее через ООП
 """
+
+from hashlib import sha256
+from uuid import uuid4
+
+
+class hex_url():
+    def __init__(self):
+        self.salt_url = {}
+        self.salt = uuid4().hex
+
+    def input_url(self):
+        url = input('Введите URL: ')
+        return self.validate_u_s(url)
+
+    def validate_u_s(self, url):
+        if self.salt_url.get(url) == None:
+            print('записи нет')
+            hexurl = sha256(self.salt.encode() + url.encode('utf-8')).hexdigest()
+            self.salt_url.update({url: hexurl})
+            print(f'Запись создана\n{self.salt_url.get(url)}')
+        else:
+            print(f'Запись существует\n{self.salt_url.get(url)}')
+        return self.input_url()
+
+
+if __name__ == "__main__":
+    hu = hex_url()
+    hu.input_url()
