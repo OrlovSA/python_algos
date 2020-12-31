43806dfe82f61760e232de459af92bbc5641a5c6
Stivin <68162679+OrlovSA@users.noreply.github.com>
2020-12-31 13:02:30 +0500

    DZ_4.1-4.5

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -15,9 +15,25 @@
 from timeit import timeit
 
 
+num_100 = [i for i in range(1000)]
+
+
 def func_1(nums):
     new_arr = []
     for i in range(len(nums)):
         if nums[i] % 2 == 0:
             new_arr.append(i)
     return new_arr
+
+
+def func_2(nums):
+    """Убрал Len, добавил подсчет итерируемых объектов"""
+    return [num for num, i in enumerate(nums) if i % 2 == 0]
+
+
+print(timeit("func_1(num_100)",
+             setup="from __main__ import func_1, num_100",
+             number=1000)) # 0.1198792
+print(timeit("func_2(num_100)",
+             setup="from __main__ import func_2, num_100",
+             number=1000)) # 0.07125669999999998
